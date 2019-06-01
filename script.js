9e29d1606b24a8cd10bade47581ced4c72579ea1
Lele992 <mic92_@msn.com>
2019-06-01 13:14:49 +0200

    fix js code

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+let round=1;
+let CPUPoint=0;
+let PlayerPoint=0;
+const roundText=document.querySelector(".selection h1");
+const buttons=Array.from(document.querySelectorAll(".choice-img"));
+buttons.forEach(button => button.addEventListener("click", playRound));
+const divPlayer=document.querySelector(".playerScore");
+const divCPU=document.querySelector(".CPUScore");
+const victory=document.querySelector(".victory");
+const points=document.querySelector(".points");
+
 function computerPlay()
 {
     let computerPick=Math.floor(Math.random()*4);
@@ -15,17 +26,25 @@ function computerPlay()
 }
 
 function getImage(selection)
-{
+{   
+    let image=document.createElement("img");
+    image.classList.add("choiceImg");
+
     switch(selection)
     {
         case "Rock":
-            return "https://static.thenounproject.com/png/477914-200.png";
-            document.getElementById("id1").style.transform = "rotate(20deg)";
+            image.style.transform="rotate(90deg)";
+            image.setAttribute("src","https://static.thenounproject.com/png/477914-200.png");
+            break;
         case "Paper":
-            return "https://sitejerk.com/images/rock-paper-scissors-png.png";
+            image.setAttribute("src","https://sitejerk.com/images/rock-paper-scissors-png.png");
+            break;
         case "Scissor":
-            return "https://static.thenounproject.com/png/477919-200.png";
+            image.setAttribute("src","https://static.thenounproject.com/png/477919-200.png");
+            break;
     }
+    
+    return image;
 }
 
 function playRound(e)
@@ -40,12 +59,8 @@ function playRound(e)
     if(CPUImg.firstChild)
         CPUImg.firstChild.remove();
     const playerPlay=e.target.getAttribute("choice");
-    let player_Img=document.createElement("img");
-    let CPU_Img=document.createElement("img");
-    player_Img.classList.add("choiceImg");
-    CPU_Img.classList.add("choiceImg");
-    player_Img.setAttribute("src",getImage(playerPlay));
-    CPU_Img.setAttribute("src",getImage(computerChoice));
+    let player_Img=getImage(playerPlay);
+    let CPU_Img=getImage(computerChoice);
     playerImg.appendChild(player_Img);
     CPUImg.appendChild(CPU_Img);
 
@@ -79,28 +94,26 @@ function playRound(e)
         points.innerHTML=PlayerPoint+" - "+CPUPoint;
         victory.innerHTML="CPU WIN!";
         roundText.innerHTML="START";
-        PlayerPoint=0;
-        CPUPoint=0;
-        divPlayer.innerHTML=PlayerPoint;
-        divCPU.innerHTML=CPUPoint;
-        playerImg.firstChild.remove();
-        CPUImg.firstChild.remove();
-        round=0;
-
+        reset();
     }else if(PlayerPoint===5){
         points.innerHTML=PlayerPoint+" - "+CPUPoint;
         victory.innerHTML="PLAYER WIN!";
-        playerImg.firstChild.remove();
-        CPUImg.firstChild.remove();
         roundText.innerHTML="START";
-        CPUPoint=0;
-        PlayerPoint=0;
-        divCPU.innerHTML=CPUPoint;
-        divPlayer.innerHTML=PlayerPoint;
-        round=0;
+        reset();
     }
 
     round++;
+    return;
+}
+
+function reset(){
+    PlayerPoint=0;
+    CPUPoint=0;
+    divPlayer.innerHTML=PlayerPoint;
+    divCPU.innerHTML=CPUPoint;
+    playerImg.firstChild.remove();
+    CPUImg.firstChild.remove();
+    round=0;
     return;
 }
 
@@ -122,14 +135,3 @@ function lose()
     divCPU.innerHTML=CPUPoint;
     return;
 }
-
-let round=1;
-let CPUPoint=0;
-let PlayerPoint=0;
-const roundText=document.querySelector(".selection h1");
-const buttons=Array.from(document.querySelectorAll(".choice-img"));
-buttons.forEach(button => button.addEventListener("click", playRound));
-const divPlayer=document.querySelector(".playerScore");
-const divCPU=document.querySelector(".CPUScore");
-const victory=document.querySelector(".victory");
-const points=document.querySelector(".points");
